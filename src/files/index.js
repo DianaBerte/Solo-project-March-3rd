b26297f1ba31095a15d359ead5cecda3c8f6faa5
DianaBerte <dianaberte.go@gmail.com>
2023-03-03 17:35:08 +0100

    Image upload fix

--- src/files/index.js
+++ src/files/index.js
@@ -1,7 +1,8 @@
 import multer from "multer";
 import Express from "express";
-import { saveProductsImgs } from "../lib/fs-tools.js";
+import { getProducts, saveProductsImgs, writeProducts } from "../lib/fs-tools.js";
 import { extname } from "path";
+import createHttpError from "http-errors";
 
 const filesRouter = Express.Router()
 
@@ -11,7 +12,18 @@ filesRouter.post("/:productId/upload", multer().single("imageUrl"), async (req, 
         const originalFileExtension = extname(req.file.originalname);
         const imgName = req.params.productId + originalFileExtension;
         await saveProductsImgs(imgName, req.file.buffer)
-        res.send({ message: "Yay, image uploaded!" })
+
+        const productsArray = await getProducts();
+        const index = productsArray.findIndex(product => product.id === req.params.productId)
+        if (index !== -1) {
+            const oldProduct = productsArray[index]
+            const updatedProduct = { ...oldProduct, "imageUrl": `http://localhost:3001/img/products/${imgName}`, updatedAt: new Date() }
+            productsArray[index] = updatedProduct
+            await writeProducts(productsArray)
+            res.send(updatedProduct)
+        } else {
+            next(createHttpError(404, `Product with id ${req.params.productId} not found!`))
+        }
     } catch (error) {
         next(error)
     }
