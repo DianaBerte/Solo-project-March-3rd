bcbf9799ab93717b752103b5ac2d893504f2fbe3
DianaBerte <dianaberte.go@gmail.com>
2023-03-03 15:54:43 +0100

    Image uploading with product's ID

--- src/files/index.js
+++ src/files/index.js
@@ -5,11 +5,11 @@ import { extname } from "path";
 
 const filesRouter = Express.Router()
 
-filesRouter.post("/upload", multer().single("imageUrl"), async (req, res, next) => {
+filesRouter.post("/:productId/upload", multer().single("imageUrl"), async (req, res, next) => {
     try {
         console.log("File:", req.file);
         const originalFileExtension = extname(req.file.originalname);
-        const imgName = req.params.id + originalFileExtension;
+        const imgName = req.params.productId + originalFileExtension;
         await saveProductsImgs(imgName, req.file.buffer)
         res.send({ message: "Yay, image uploaded!" })
     } catch (error) {
